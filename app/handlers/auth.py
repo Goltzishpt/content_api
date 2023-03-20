8b85e8a438e4395c2e6042d3a3c4fef542aa8a10
Goltz <95189224+Goltzishpt@users.noreply.github.com>
2023-03-20 19:29:56 +0200

    upd login

--- app/handlers/auth.py
+++ app/handlers/auth.py
@@ -1,7 +1,9 @@
+import psycopg2
 from flask import jsonify, request
-from app.utils import get_password_hash
 from app.loader import app
 from app.models import User
+from app.utils import get_password_hash
+from app.sessions import set
 
 
 def homepage():
@@ -17,8 +19,28 @@ def registration():
 
 def login():
     data = request.get_json()
-    data['password'] = get_password_hash(data['password'])
-    return jsonify(data)
+    username = data['login']
+    password = get_password_hash(data['password'])
+    print(password)
+    conn = psycopg2.connect(database="ps_db", user="test",
+                            password="admin", host="localhost", port=5432)
+    try:
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, password FROM "user" WHERE login =%s', (username,))
+        cur_data = cursor.fetchone()
+        if cur_data is not None:
+            print(cur_data)
+            if cur_data[1] == password:
+
+                out = jsonify(state=0, msg='success')
+                out.set_cookie('token', set(cur_data[0]))
+                return out
+            else:
+                return 'Incorrect password!'
+        else:
+            return 'User not found!'
+    finally:
+        conn.close()
 
 
 def logout():
--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,13 @@ from hashlib import sha256
 
 def get_password_hash(password: str) -> str:
     return sha256(password.encode('utf-8')).hexdigest()
+
+
+class Validator:
+    username: str = False
+    phone: str = False
+    login: str = False
+    password: str = False
+
+    def validator_login(self):
+        pass
